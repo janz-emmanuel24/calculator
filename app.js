a4c2c87320a2dcb659a3d35b98fd6a5909e4a48f
Jansuk Emmanuel <jansabbas1@gmail.com>
2023-03-02 10:44:29 +0300

    add database connection

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const logger = require('./logger/calcLogger')
+const mongoose = require('mongoose')
 
 const app = express()
 
 logger.info('Calculator opened')
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+}).then(con => {
+    console.log(con.connections)
+    console.log('DB connection successful')
+})
+
+const calcSchema = new mongoose.Schema({
+    calResult: Number
+})
+
+const Result = mongoose.model('CalculatedResults', calcSchema)
+
+const testResults = new Result({
+    calResult: 788
+})
+
+testResults.save().then(doc => {
+    console.log(doc)
+})
 
 //pug config
 app.set('view engine', 'pug')
